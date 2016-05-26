e2c495654d1221443656669bc8c94174432098c9
Dan <danielandrewvoss@gmail.com>
2016-05-26 12:57:27 -0400

    Add HashMap

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -1,8 +1,14 @@
 package com.dvoss;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        HashMap<String, ArrayList<Country>> map = new HashMap<>();
+
+
     }
 }
