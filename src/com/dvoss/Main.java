f1fe70c293ce73c45463c196d3d2819b45abe0b9
Dan <danielandrewvoss@gmail.com>
2016-05-26 15:09:28 -0400

    Complete HashMap

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -11,24 +11,18 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         HashMap<String, ArrayList<Country>> map = new HashMap<>();
-        //ArrayList<Country> countries = new ArrayList<>();
         File f = new File("countries.txt");
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
             String[] columns = line.split("\\|");
             Country country = new Country(columns[0], columns[1]);
-//            countries.add(country);
-//            for (Country cntry : countries) {
                 if (!map.containsKey(String.valueOf(country.name.charAt(0)))) {
                     map.put(String.valueOf(country.name.charAt(0)), new ArrayList<>());
                 }
-                //else {
-                    ArrayList<Country> countries = map.get(String.valueOf(country.name.charAt(0)));
-                    countries.add(country);
-                //}
-
+                ArrayList<Country> countries = map.get(String.valueOf(country.name.charAt(0)));
+                countries.add(country);
         }
-        System.out.println(map);
+        //System.out.println(map);
     }
 }
