db0963d6acba1e930f7bf539e8be039948d08cec
Dan <danielandrewvoss@gmail.com>
2016-05-26 12:38:07 -0400

    Create project and add Country class

---
+++ src/com/dvoss/Country.java
@@ -0,0 +1,32 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 5/26/16.
+ */
+public class Country {
+    String abbreviation;
+    String name;
+
+    public Country(String abbreviation, String name) {
+        this.abbreviation = abbreviation;
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
