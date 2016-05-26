6eb00a6249a8dc7edf2d1eed034b1cf56d8038e3
Dan <danielandrewvoss@gmail.com>
2016-05-26 15:30:06 -0400

    Add user save file

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -2,13 +2,15 @@ package com.dvoss;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         HashMap<String, ArrayList<Country>> map = new HashMap<>();
         File f = new File("countries.txt");
@@ -24,5 +26,14 @@ public class Main {
                 countries.add(country);
         }
         //System.out.println(map);
+
+        System.out.println("Type a letter to save a list of countries starting with that letter.");
+        Scanner scanner = new Scanner(System.in);
+        // if not a single letter, throw exception
+        String letter = scanner.nextLine();
+        File c = new File("&s_countries.txt\n", letter);
+        FileWriter fw = new FileWriter(c);
+        //fw.write();
+
     }
 }
