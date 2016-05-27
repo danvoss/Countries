3a32a6ccbea38ba834cf5e2fa37cf2b32d8e58da
Dan <danielandrewvoss@gmail.com>
2016-05-27 09:48:56 -0400

    Attempt to refactor

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -10,37 +10,77 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    // parseCountries method
 
+    public static HashMap<String, ArrayList<Country>> parseCountries(String filename) throws FileNotFoundException {
         HashMap<String, ArrayList<Country>> map = new HashMap<>();
-        File f = new File("countries.txt");
+        File f = new File(filename);
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
             String[] columns = line.split("\\|");
             Country country = new Country(columns[0], columns[1]);
-                if (!map.containsKey(String.valueOf(country.name.charAt(0)))) {
-                    map.put(String.valueOf(country.name.charAt(0)), new ArrayList<>());
-                }
-                ArrayList<Country> countries = map.get(String.valueOf(country.name.charAt(0)));
-                countries.add(country);
+            if (!map.containsKey(String.valueOf(country.name.charAt(0)))) {
+                map.put(String.valueOf(country.name.charAt(0)), new ArrayList<>());
+            }
+            ArrayList<Country> countries = map.get(String.valueOf(country.name.charAt(0)));
+            countries.add(country);
         }
-        //System.out.println(map);
+        return map;
+    }
 
+    //readInputWriteFile method
+
+    public static void readInputWriteFile(HashMap<String, ArrayList<Country>> map) throws Exception {
         System.out.println("Type a letter to save a list of countries starting with that letter.");
         Scanner scanner = new Scanner(System.in);
-        // if not a single letter, throw exception
         String letter = scanner.nextLine();
         if (letter.length() != 1) {
             throw new Exception("Please type a single letter.");
-        }
-        else {
+        } else {
             File c = new File(letter + "_countries.txt");
             FileWriter fw = new FileWriter(c);
             ArrayList<Country> list = map.get(letter);
             fw.write(list.toString());
             fw.close();
         }
-        //System.out.println(map.get(letter));
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        HashMap<String, ArrayList<Country>> map = parseCountries("countries.txt");
+
+        readInputWriteFile(map);
+
+//        HashMap<String, ArrayList<Country>> map = new HashMap<>();
+//        File f = new File("countries.txt");
+//        Scanner fileScanner = new Scanner(f);
+//        while (fileScanner.hasNext()) {
+//            String line = fileScanner.nextLine();
+//            String[] columns = line.split("\\|");
+//            Country country = new Country(columns[0], columns[1]);
+//                if (!map.containsKey(String.valueOf(country.name.charAt(0)))) {
+//                    map.put(String.valueOf(country.name.charAt(0)), new ArrayList<>());
+//                }
+//                ArrayList<Country> countries = map.get(String.valueOf(country.name.charAt(0)));
+//                countries.add(country);
+//        }
+        //System.out.println(map);
+
+//        System.out.println("Type a letter to save a list of countries starting with that letter.");
+//        Scanner scanner = new Scanner(System.in);
+//        String letter = scanner.nextLine();
+//        if (letter.length() != 1) {
+//            throw new Exception("Please type a single letter.");
+//        }
+//        else {
+//            File c = new File(letter + "_countries.txt");
+//            FileWriter fw = new FileWriter(c);
+//            ArrayList<Country> list = map.get(letter);
+//            fw.write(list.toString());
+//            fw.close();
+//        }
+//        //System.out.println(map.get(letter));
+//    }
     }
 }
