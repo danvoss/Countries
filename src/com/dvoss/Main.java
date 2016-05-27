0c302bad91309bd2a2d2703c8840637a18583315
Dan <danielandrewvoss@gmail.com>
2016-05-27 13:30:17 -0400

    Clean up and complete

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -5,7 +5,6 @@ import jodd.json.JsonSerializer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
